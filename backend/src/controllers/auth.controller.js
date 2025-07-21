a52925a39d9510e1b7200ea15131df8485b60ccc
audrio <zerotwo8802@gmail.com>
2025-07-21 11:21:14 +0700

    make authentication route

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,33 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
+export const signup = async (req, res) => {
+    const {fulName, email, password} = req.body;
+    try{
+        
+        if(password.length < 6){
+            return res.status(400).json({
+                message: "Password mush be at least 6 characters"
+            })
+        }
+
+        const user = await User.findOne({email});
+
+        if(user) return res.status(400).json({
+            message: "Email already exists"
+        });
+        
+        // hash password using bcrypt
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt)
+    } catch(error){
+
+    }
+    res.send("signup route");
+}
+export const login = (req, res) => {
+    res.send("login route");
+}
+export const logout = (req, res) => {
+    res.send("logout route");
+}
